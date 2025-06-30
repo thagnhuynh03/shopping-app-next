0651070b70d9054ec968c1d622faba30a7429edf
Kim Thang <thanghuynh.271203@gmail.com>
2025-06-30 20:21:35 +0700

    Remote pattern image

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'shopping-app-nest.onrender.com',
-      //   pathname: '/images/**',
-      // },
       {
-        hostname: 'localhost'
+        protocol: 'https',
+        hostname: 'shopping-app-nest.onrender.com',
+        pathname: '/images/**',
       },
+      // {
+      //   hostname: 'localhost'
+      // },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
