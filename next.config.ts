7be22afca382697fbffd855cafea28cf8f4e3753
Kim Thang <thanghuynh.271203@gmail.com>
2025-07-21 16:28:04 +0700

    Fix image remote

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // {
-      //   protocol: 'https',
-      //   hostname: 'shopping-app-nest.onrender.com',
-      //   pathname: '/images/**',
-      // },
+      {
+        protocol: 'https',
+        hostname: 'shopping-app-nest.onrender.com',
+        pathname: '/images/**',
+      },
       {
         hostname: 'localhost'
       },
